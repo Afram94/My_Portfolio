01e80014091265b7d421166be4dc838c26a9e272
Afram94 <afram.h@hotmail.com>
2023-09-07 23:33:58 +0200

    Work experience section is done

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -14,7 +14,7 @@ const ExperienceCard = ({ experiences }) => (
     date={experiences.date}
     iconStyle={{ background: experiences.iconBg }}
     icon={
-      <div>
+      <div className="flex justify-center items-center w-full h-full">
         <img 
           src={experiences.icon}
           alt={experiences.company_name}
@@ -27,7 +27,19 @@ const ExperienceCard = ({ experiences }) => (
         <h3 className="text-white text-[24px] font-bold">
           {experiences.title}
         </h3>
+        <p className="text-secondary text-[16px] font-semibold" style={{margin: 0}}>
+          {experiences.company_name}
+        </p>
       </div>
+      <ul className="mt-5 list-disc ml-5 space-y-2">
+        {experiences.points.map((point, index) => (
+          <li key={`experiences.points-${index}`}
+              className="text-white-100 text-[14px] pl-1 tracking-wider"
+          >
+            {point}
+          </li>
+        ))}
+      </ul>
   </VerticalTimelineElement>
 )
 
