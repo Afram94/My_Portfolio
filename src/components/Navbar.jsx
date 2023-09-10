1bb4121659678c2d03de83364d3ac4578a684b8c
Afram94 <afram.h@hotmail.com>
2023-09-10 20:06:53 +0200

    Works section is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
         <p className='text-white text-[18px] font-bold cursor-pointer flex'>
           Afram &nbsp;
           <span className='sm:block hidden'>
-           | JS Mastery</span>
+           | Portfolio</span>
         </p>
         </Link>
         <ul className='list-none hidden sm:flex flex-row gap-10'>
