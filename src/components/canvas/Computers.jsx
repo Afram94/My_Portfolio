b50a51cb1ccd7c183423ecca57d05cee93c9714d
Afram94 <afram.h@hotmail.com>
2023-09-19 20:56:47 +0200

    Testimonials section is done. Added some comments in Computers component

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -7,23 +7,26 @@ const Computers = ({isMobile}) => {
   const computer = useGLTF('./desktop_pc/scene.gltf')
   
   return (
-    <mesh>
-      <hemisphereLight intensity={0.15}
-        groundColor= "black" />
-        <pointLight intensity={1} />
-        <spotLight position={[-20, 50, 10]}
-        angle={0.12}
-        penumbra={1}
-        intensity={1}
-        castShadow
-        shadow-mapSize={1024}
-        />
-          <primitive
-          object={computer.scene}
-          scale={isMobile ? 0.7 : 0.75}
-          position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}
-          rotation={[-0.01, -0.2, -0.1]}
-          />
+    <mesh>  // Container for grouping 3D objects
+      <hemisphereLight
+        intensity={0.15}  // Amount of light emitted
+        groundColor= "black" // Color of the light from below
+        />  
+      <pointLight intensity={1} // Amount of light emitted in all directions
+      />  
+      <spotLight position={[-20, 50, 10]}  // Position of the spotlight in the 3D space
+        angle={0.12}  // Angle of the light cone
+        penumbra={1}  // Softness of the light's edge
+        intensity={1}  // Amount of light emitted
+        castShadow  // Enables casting shadows
+        shadow-mapSize={1024}  // Quality of the shadows
+      />
+      <primitive  // Renders the 3D model
+        object={computer.scene}  // The loaded 3D model
+        scale={isMobile ? 0.7 : 0.75}  // Size of the model, adjusted for mobile/desktop
+        position={isMobile ? [0, -3, -2.2] : [0, -3.25, -1.5]}  // Position of the model in 3D space
+        rotation={[-0.01, -0.2, -0.1]}  // Rotation angles (in radians) for the model
+      />
     </mesh>
   )
 }
@@ -57,21 +60,21 @@ const ComputersCanvas = () => {
 
   return (
     <Canvas
-    frameloop="demand"
-    shadows
-    camera={{position:[20, 3, 5], fov: 25}}
-    gl={{preserveDrawingBuffer: true}}
+      frameloop="demand"  // Determines how often the scene is re-rendered; "demand" means only when necessary
+      shadows  // Enables shadow rendering
+      camera={{position:[20, 3, 5], fov: 25}}  // Camera properties like position and field of view
+      gl={{preserveDrawingBuffer: true}}  // WebGL setting; retains the buffer for further use
     >
       <Suspense fallback={<CanvasLoader />}>
-        <OrbitControls
-          enableZoom={false}
-          maxPolarAngle={Math.PI / 2}
-          minAzimuthAngle={Math.PI / 2}
+        <OrbitControls  // Provides controls for orbiting the scene with mouse or touch
+          enableZoom={false}  // Disables zooming functionality
+          maxPolarAngle={Math.PI / 2}  // Limits the maximum orbit angle vertically
+          minAzimuthAngle={Math.PI / 2}  // Limits the minimum orbit angle horizontally
         />
         <Computers isMobile={isMobile}/>
       </Suspense>
-
-      <Preload all/>
+      <Preload all // Preloads all assets in the scene for faster rendering
+      />  
     </Canvas>
   )
 }
